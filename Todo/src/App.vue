55392cbfe4edc719694e3e994250537c3647a4dc
Noidea22 <mathewsteenwinkel@gmail.com>
2024-12-16 14:56:32 -0700

    initial set up

---
+++ Todo/src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+import TodoList from './components/todoList.vue';
+
+
+</script>
+
+<template>
+  <header>
+   <TodoList/>
+
+
+  </header>
+
+</template>
+
+<style scoped>
+header{
+  font-size: 100px;
+}
+</style>
---
+++ Todo/src/components/todoList.vue
@@ -0,0 +1,27 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="header">
+   VUE todo
+   <h1>
+     make a todo list in my vue todo app!
+    </h1>
+  </div>
+
+</template>
+
+<style scoped>
+.header{
+  font-size: 100px;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;  
+}
+h1{
+  font-size: 40px;
+}
+</style>
